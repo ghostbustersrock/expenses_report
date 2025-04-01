9be24354dafed0b63504e8424c2372177dc05962
Luca Santarelli <luca.santarelli@Lucas-Laptop.local>
2025-04-01 17:20:41 +0100

    fix alembic migrations with tables and foreing key contstraints

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+import os
+
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+path = os.path.abspath(os.path.expanduser("./app/templates"))
+templates = Jinja2Templates(directory=path)
+
 
 @app.get("/")
-def homepage():
-    return {"Hello": "World"}
+def homepage(request: Request):
+
+    return templates.TemplateResponse("homepage.html", {"request": request})
--- expenses_report/models.py
+++ expenses_report/models.py
@@ -1,14 +1,27 @@
 from core.db.database import Base
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, INTEGER, TEXT, DATE, NUMERIC, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class User(Base):
-    __tablename__ = "test_table"
+class Expenses(Base):
+    __tablename__ = "expenses"
 
-    id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(
-        String,
+    id = Column(INTEGER, primary_key=True, index=True)
+    expense_category_id = Column(
+        INTEGER, ForeignKey("expenses_categories.category_id"), nullable=False
     )
-    last_name = Column(String)
-    age = Column(Integer)
+    expense_month = Column(TEXT, nullable=False)
+    expense_year = Column(NUMERIC, nullable=False)
+    expense_cost = Column(NUMERIC, nullable=False)
+
+    expense_category = relationship("ExpensesCategories", back_populates="expenses")
+
+
+class ExpensesCategories(Base):
+    __tablename__ = "expenses_categories"
+
+    category_id = Column(INTEGER, primary_key=True, index=True)
+    category_type = Column(TEXT, nullable=False)
+
+    expenses = relationship("Expenses", back_populates="expense_category")
